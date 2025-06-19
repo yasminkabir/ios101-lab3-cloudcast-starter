a32748fd751f8749e49f98d283367471e0ca3102
Yasmin Kabir <yasminkabirofficial@gmail.com>
2025-06-18 18:31:39 -0700

    fixed after test

--- CloudCast/ForecastViewController.swift
+++ CloudCast/ForecastViewController.swift
@@ -11,7 +11,6 @@ class ForecastViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hi")
         // Do any additional setup after loading the view.
     }
     
